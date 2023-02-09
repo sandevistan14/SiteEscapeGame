acaf058983268a9839ecda4463f399871c624ccf
Robin <robin.lerouge@etu.univ-amu.fr>
2023-02-09 14:40:52 +0100

    logo
    Co-authored-by: Rackyrr <Rackyrr@users.noreply.github.com>
    Co-authored-by: Michaël Ditlecadet <MichaelDitlecadet@users.noreply.github.com>

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,10 +1,1 @@
-//code pour la saisie d'un code secret 
-/*
-var code = prompt("Entrez le code secret");
-if (code == "1234") {
-    alert("Bienvenue");
-}
-else {
-    alert("Accès refusé");
-}
-*/
+
