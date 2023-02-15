89f512bfca94c13b0d03bf09141209255fd085f8
Rackyrr <valentin.lestrelin@free.fr>
2023-02-15 12:09:10 +0100

    Avancement site + debut énigme

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,1 +1,14 @@
-
+function Password() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = "1234";
+    passwordInput.addEventListener("keydown", function (event) { 
+        if (event.key == "Enter") {
+            if (password != confirmPassword) {
+                alert("Ce n'est pas le bon mot de passe");
+                return false;
+            }
+            document.location.href = "asset/html/niveau2.html";
+            return true;
+        }
+    });
+}
