67bff1276e4d34306656510606200f556208b00f
Rackyrr <valentin.lestrelin@free.fr>
2023-02-16 12:06:45 +0100

    avancement des énigmes

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,14 +1,17 @@
+function EnterPressed() {
+    var key = window.event.keyCode;
+    if (key == 13) {
+        Password();
+    }
+}
+
 function Password() {
     var password = document.getElementById("password").value;
     var confirmPassword = "1234";
-    passwordInput.addEventListener("keydown", function (event) { 
-        if (event.key == "Enter") {
-            if (password != confirmPassword) {
-                alert("Ce n'est pas le bon mot de passe");
-                return false;
-            }
-            document.location.href = "asset/html/niveau2.html";
-            return true;
-        }
-    });
+    if (password != confirmPassword) {
+        alert("Ce n'est pas le bon mot de passe");
+        return false;
+    }
+    document.location.href = "asset/html/niveau2.html";
+    return true;
 }
