37d4c42ba4f4478cb66b5b4c5dbc2d54732dcc0a
Robin <robin.lerouge@etu.univ-amu.fr>
2023-02-17 03:01:03 +0100

    finalisation site
    Co-authored-by: Rackyrr <Rackyrr@users.noreply.github.com>
    Co-authored-by: Michaël Ditlecadet <MichaelDitlecadet@users.noreply.github.com>

--- asset/javascript/script.js
+++ asset/javascript/script.js
@@ -1,46 +1,46 @@
 function EnterPressed() {
-    var key = window.event.keyCode;
-    if (key == 13) {
-        Password();
-    }
+   var key = window.event.keyCode;
+   if (key == 13) {
+      Password();
+   }
 }
 
 function Password() {
-    var password = document.getElementById("password").value;
-    var confirmPassword = "1234";
-    if (password != confirmPassword) {
-        alert("Ce n'est pas le bon mot de passe");
-        return false;
-    }
-    document.location.href = "../html/niveau2.html";
-    return true;
+   var password = document.getElementById("password").value;
+   var confirmPassword = "3141592653";
+   if (password != confirmPassword) {
+      alert("Ce n'est pas le bon mot de passe");
+      return false;
+   }
+   document.location.href = "../html/niveau2.html";
+   return true;
 }
 
 //Timer
 
 TweenLite.defaultEase = Expo.easeOut;
 
-initTimer("10:01"); // exemples --> "0:15" "03:5" "5:2"
+initTimer("05:00"); // other ways --> "0:15" "03:5" "5:2"
 
 var reloadBtn = document.querySelector('.reload');
 var timerEl = document.querySelector('.timer');
 
-function initTimer (t) {
-   
+function initTimer(t) {
+
    var self = this,
-       timerEl = document.querySelector('.timer'),
-       minutesGroupEl = timerEl.querySelector('.minutes-group'),
-       secondsGroupEl = timerEl.querySelector('.seconds-group'),
+      timerEl = document.querySelector('.timer'),
+      minutesGroupEl = timerEl.querySelector('.minutes-group'),
+      secondsGroupEl = timerEl.querySelector('.seconds-group'),
 
-       minutesGroup = {
-          firstNum: minutesGroupEl.querySelector('.first'),
-          secondNum: minutesGroupEl.querySelector('.second')
-       },
+      minutesGroup = {
+         firstNum: minutesGroupEl.querySelector('.first'),
+         secondNum: minutesGroupEl.querySelector('.second')
+      },
 
-       secondsGroup = {
-          firstNum: secondsGroupEl.querySelector('.first'),
-          secondNum: secondsGroupEl.querySelector('.second')
-       };
+      secondsGroup = {
+         firstNum: secondsGroupEl.querySelector('.first'),
+         secondNum: secondsGroupEl.querySelector('.second')
+      };
 
    var time = {
       min: t.split(':')[0],
@@ -69,10 +69,10 @@ function initTimer (t) {
       timeNumbers = timestr.split('');
       updateTimerDisplay(timeNumbers);
 
-      if(timestr === '0000')
+      if (timestr === '0000')
          countdownFinished();
 
-      if(timestr != '0000')
+      if (timestr != '0000')
          setTimeout(updateTimer, 1000);
 
    }
@@ -86,7 +86,7 @@ function initTimer (t) {
 
    }
 
-   function animateNum (group, arrayValue) {
+   function animateNum(group, arrayValue) {
 
       TweenMax.killTweensOf(group.querySelector('.number-grp-wrp'));
       TweenMax.to(group.querySelector('.number-grp-wrp'), 1, {
@@ -94,7 +94,7 @@ function initTimer (t) {
       });
 
    }
-   
+
    setTimeout(updateTimer, 1000);
 
 }
@@ -103,11 +103,17 @@ function countdownFinished() {
    setTimeout(function () {
       TweenMax.set(reloadBtn, { scale: 0.8, display: 'block' });
       TweenMax.to(timerEl, 1, { opacity: 0.2 });
-      TweenMax.to(reloadBtn, 0.5, { scale: 1, opacity: 1 }); 
+      TweenMax.to(reloadBtn, 0.5, { scale: 1, opacity: 1 });
    }, 1000);
 }
 
 reloadBtn.addEventListener('click', function () {
-   TweenMax.to(this, 0.5, { 
+   TweenMax.to(this, 0.5, {
+      opacity: 0, onComplete:
+         function () {
+            reloadBtn.style.display = "none";
+         }
    });
+   TweenMax.to(timerEl, 1, { opacity: 1 });
+   initTimer("12:35");
 });
